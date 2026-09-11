4fb174a82963cbfad91026686471d1104531b4b4
João Paulo M. Votri <jpaulo.votri@hotmail.com>
2026-09-11 19:14:49 -0300

    feat(backend): Commit inicial

---
+++ backend/MedievalApi/Program.cs
@@ -0,0 +1,26 @@
+using Scalar.AspNetCore;
+
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.MapScalarApiReference();
+        }
+
+        app.MapControllers();
+        app.Run();
+    }
+}
